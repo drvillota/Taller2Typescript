3a0bae1b0f0c0709d36d49d3a199bc7862d9b778
David Ruiz <dr.villota@uniandes.edu.co>
2020-03-31 17:00:09 -0500

    Taller 1

---
+++ dataCourses.js
@@ -0,0 +1,5 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("SisTrans", "German Bravo", 3),
+    new Course("Desarrollo de SW", "Cesar Forero", 3)
+];
---
+++ dataCourses.ts
@@ -0,0 +1,6 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("SisTrans", "German Bravo", 3),
+  new Course("Desarrollo de SW", "Cesar Forero", 3)
+]
